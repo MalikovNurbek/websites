b5e65535122a0bcbed682c85e33d0c613a656325
MalikovNurbek <malikov.n004@gmail.com>
2022-01-11 18:09:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,129 @@
+//============================== Массивы - ARRAY'S =========================================
+
+
+// var array = [];
+// var fruits = ['Яблоко', 'Груша', 'Апельсин'];
+// console.log(fruits[2]);
+
+// var num = [23, 54, 109 , 23, 786, 23];
+// console.log(num[4]);
+// var num = [23, 54, 109 , 23, 786, 23];
+// console.log(num);
+
+// var arr = [true, false, false, true];
+// var arr2 = [true, 44, false, 'Hello' ,false, null ,true, undefined];
+
+// ============ В JavaScript можно писать любые типы данных!
+
+// var employeeId = [435, 345, 235, 234, 252]
+
+// console.log(employeeId);
+
+// employeeId[1] = 12
+
+// console.log(employeeId);
+
+// employeeId[6] = 23
+// console.log(employeeId);
+// employeeId[5] = 34
+// console.log(employeeId);
+
+//====================================== delete employeeId[4]
+// console.log(employeeId);
+
+// var students = ['Omad', 'Bekzat', 'Ali', 'Madina', 'Maxim', 'Islom']
+
+// =========== length  - длина текста
+
+
+// console.log(students[2].length);
+
+// for(var i = 0; i < students.length; i++){
+//     console.log(students[i]);
+
+//     if(students[i] === 'Ali'){
+//         continue
+//     }else{
+//         console.log(students[i]);
+//     }
+// }
+
+// var num = [435, 345, 235, 234, 252]
+// console.log(num.length);
+// num.length = 2
+// console.log(num.length);
+
+
+// var nums = [Math.floor(Math.random() * (15 - (-15)) + (-15))]
+// nums.length = 12
+// nums[[Math.floor(Math.random() * (15 - (-15)) + (-15))]]
+// console.log(nums);
+// =========================================
+
+// var arr = []
+// result = 0 
+// for(var i = 0; i < 12; i++){
+//     arr[i] = Math.floor(Math.random() * (15 - (-15)) + (-15))
+//     if(result < arr[i]){
+//         result = arr[i]
+//     } else {
+//         continue
+//     }
+// }
+// console.log(arr);
+// console.log(result);
+
+// =========================================
+
+
+// var arr = []
+// for(var i = 0; i < 8; i++){
+//     arr[i] = Math.floor(Math.random() * (15 - (-15)) + (-15))
+//     if(arr[i] % 2 !== 0){
+//         arr[i] = 0
+//     }
+
+// }
+// console.log(arr);
+// console.log(Math.m(arr));
+
+// =========================================
+// var arr = []
+// for(var i = 0; i < 5; i++){
+//     arr[i] = Math.floor(Math.random() * (5 - (0)) + 0)
+// } 
+// var arr2 = []
+// for(var i = 0; i < 5; i++){
+//     arr2[i] = Math.floor(Math.random() * (5 - (0)) + 0)
+// } 
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr + arr2 / (arr.length + arr2.length));
+
+// ===================================================================
+
+
+var arr = +prompt('Введите длину')
+var arr2 = 0;
+// var num5 = 0
+console.log(arr2);
+for (var i = 0; i < arr2; i++) {
+    arr2[i] = Math.floor(Math.random() * (5 - (0)) + 0)
+
+    if (arr.length >= 3 && arr.length > 0) {
+        arr.length
+        arr2.length = arr
+        break
+    } else {
+        arr
+    }
+}
+console.log(arr2.length); 
+// for (var j = 0; j < arr.length; j++){
+//     if(arr[j] % 2 !== 0){
+//         continue
+//     }
+// }
+// console.log(arr[i]);
+
